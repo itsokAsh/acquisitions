3dcdb26b8172cc248ea2c2887039eda3c55485a5
itsokAsh <ashezz0512@gmail.com>
2026-06-11 15:40:16 +0530

    Implement Dockerization

--- src/config/database.js
+++ src/config/database.js
@@ -4,6 +4,12 @@ import {neon} from '@neondatabase/serverless';
 
 import {drizzle} from 'drizzle-orm/neon-http';
 
+if(process.env.NODE_ENV === 'development'){
+    neonConfig.fetchEndpoint = 'http://neon-local:5432/sql';
+    neonConfig.useSecureWebsocket = false;
+    neonConfig.poolQueryViaFetch = true;
+}
+
 const sql = neon(process.env.DATABASE_URL);
 const db = drizzle(sql);
 
